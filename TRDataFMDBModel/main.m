aafd30a1cfcac5310a2142fee18ab503b6468214
张元武 <674707239@qq.com>
2017-12-21 17:32:45 +0800

    Initial Commit

---
+++ TRDataFMDBModel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TRDataFMDBModel
+//
+//  Created by ZYW on 2017/12/21.
+//  Copyright © 2017年 ZYW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
