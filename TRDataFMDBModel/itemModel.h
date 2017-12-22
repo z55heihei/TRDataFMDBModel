5055b22b21a7c1d68f5539210b0cef2f3e1db018
张元武 <674707239@qq.com>
2017-12-22 15:02:06 +0800

    demo

---
+++ TRDataFMDBModel/itemModel.h
@@ -0,0 +1,21 @@
+//
+//  ActivityModel.h
+//  TRDataFMDBModel
+//
+//  Created by ZYW on 2017/12/22.
+//  Copyright © 2017年 ZYW. All rights reserved.
+//
+
+#import "TRDataFMDBModel.h"
+
+@interface ActivityModel : TRDataFMDBModel
+
+@property (nonatomic,copy) NSString *productDetail;
+@property (nonatomic,copy) NSString *productLoadingurl;
+@property (nonatomic,copy) NSString *productTitle;
+@property (nonatomic,copy) NSString *productSubicon;
+@property (nonatomic,copy) NSString *productId;
+@property (nonatomic,copy) NSString *productIcon;
+@property (nonatomic,copy) NSString *productUrl;
+
+@end
---
+++ TRDataFMDBModel/itemModel.m
@@ -0,0 +1,17 @@
+//
+//  ActivityModel.m
+//  TRDataFMDBModel
+//
+//  Created by ZYW on 2017/12/22.
+//  Copyright © 2017年 ZYW. All rights reserved.
+//
+
+#import "ActivityModel.h"
+
+@implementation ActivityModel
+
+- (NSString *)setPrimaryKey{
+	return @"productId";
+}
+
+@end
